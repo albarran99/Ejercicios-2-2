d1433593cffe75e04179b94bbe61dba3ab93865b
raulalbarran <raulvezon15@gmail.com>
2021-09-23 13:02:00 +0200

    update

--- LeerDeResources(actualizado)/src/main/java/org/iesfm/readerText/ejer3/FileToStringReaderQuijote.java
+++ LeerDeResources(actualizado)/src/main/java/org/iesfm/readerText/ejer3/FileToStringReaderQuijote.java
@@ -1,9 +1,20 @@
 package org.iesfm.readerText.ejer3;
 
-import java.io.File;
+import java.io.*;
 
 public class FileToStringReaderQuijote {
     public String readFile(File file) {
         StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
     }
 }
---
+++ LeerDeResources(actualizado)/src/main/java/org/iesfm/readerText/ejer3/QuijoteReader.java
@@ -0,0 +1,16 @@
+package org.iesfm.readerText.ejer3;
+
+import org.iesfm.readerText.claseEjer2ConPoo.FolderReader;
+
+import java.io.File;
+
+public class QuijoteReader {
+    public static void main(String[] args) {
+
+        File classpathFile = new File(this.getClass().getResource("/log4j.xml").toURI());
+        classpathFile
+
+
+    }
+    public String readAll(String folder)
+}
